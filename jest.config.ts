c1ee0381372353df45a5389af8e31b82ebd6ab26
Jay <jaykumar1999k@gmail.com>
2026-09-07 15:18:04 +0530

    jest setup

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  // 👈 Explicit fallback mapping for Jest
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+}
+
+export default createJestConfig(config)
